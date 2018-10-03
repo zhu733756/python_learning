f323d13dcab30b9b21d1cd2c374f2ef1de2c492d
zhu733756 <1079333812@qq.com>
2018-10-04 03:20:30 +0800

    update

--- WriterHelper/Novel/views.py
+++ WriterHelper/Novel/views.py
@@ -13,15 +13,22 @@ def search_dir(request):
     idioms_path = SearchTools.SearchRes.find_searchKey("idiom")
     novels_path = SearchTools.SearchRes.find_searchKey("verb")
     idioms_path.extend(novels_path)
-    for path in idioms_path:
-        if req in path:
+    if req:
+        for path in idioms_path:
+            if req in path:
+                author,book=os.path.split(os.path.dirname(path))[-1].split("-")[:]
+                if author in info:
+                    if book in info[author]:
+                        continue
+                info.setdefault(author,[]).append(book)
+    else:
+        for path in idioms_path:
             author,book=os.path.split(os.path.dirname(path))[-1].split("-")[:]
             if author in info:
                 if book in info[author]:
                     continue
             info.setdefault(author,[]).append(book)
     if info:
-        print(info)
         return JsonResponse(info)
     else:
         return JsonResponse("没有检测到可用数据！",safe=False)
